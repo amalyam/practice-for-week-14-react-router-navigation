5935a50ff1ac39d8acfd4d43d68beefd4274b924
Amalya Megerman <amalyamegerman@gmail.com>
2023-02-21 12:11:27 -0500

    add handleClick + useHistory

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,13 +1,20 @@
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 
 function Stocks() {
-  let loggedIn = false;
+  let loggedIn = true;
+  let history = useHistory();
   if (!loggedIn) {
-    <Redirect to="/not-logged-in" />;
+    return <Redirect to="/not-logged-in" />;
   }
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+    console.log(history);
+  };
   return (
     <div className="comp orange">
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
